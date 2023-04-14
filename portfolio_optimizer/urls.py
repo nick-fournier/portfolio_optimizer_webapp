bd36e04babb0a9cc62eb67cb33389918961e14de
nick-fournier <nichfournier@gmail.com>
2023-04-13 22:48:41 -0700

    major cleanup

--- portfolio_optimizer/urls.py
+++ portfolio_optimizer/urls.py
@@ -12,8 +12,6 @@ urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('dashboard/', DashboardView.as_view(), name='dashboard'),
     path('add-data/', AddDataView.as_view(), name='add-data'),
-    # path('dashboard/add-data/', AddDataView.as_view(), name='add-data'),
-    # path('dashboard/optimize/', OptimizeView.as_view(), name='optimize'),
     path('accounts/', include('django.contrib.auth.urls')),
     path('api/', include(router.urls))
 ]
