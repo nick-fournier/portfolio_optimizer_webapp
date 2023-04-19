66c1a070255e684cf49d5f21e025d0a29c0acbe2
nick-fournier <nichfournier@gmail.com>
2023-04-19 09:29:45 -0700

    update url tags

--- portfolio_optimizer/urls.py
+++ portfolio_optimizer/urls.py
@@ -9,9 +9,9 @@ router.register(r'data-settings/1', DataSettingsSerializerView, 'data-settings')
 
 urlpatterns = [
     # path('', index, name='index'),
-    path('', IndexView.as_view(), name='index'),
-    path('dashboard/', DashboardView.as_view(), name='dashboard'),
-    path('add-data/', AddDataView.as_view(), name='add-data'),
+    path('', IndexView.as_view(), name='portfolio-optimizer-index'),
+    path('dashboard/', DashboardView.as_view(), name='portfolio-optimizer-dashboard'),
+    path('add-data/', AddDataView.as_view(), name='portfolio-optimizer-add-data'),
     path('accounts/', include('django.contrib.auth.urls')),
     path('api/', include(router.urls))
 ]
